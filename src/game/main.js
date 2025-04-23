ba3765cb1dcb4901e91ca20f9c6450d763f10b90
Francisco Pereira Alvarado <gammafp@gmail.com>
2025-04-23 15:33:30 +0200

    clean scenes

--- src/game/main.js
+++ src/game/main.js
@@ -5,8 +5,8 @@ import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
 import { AUTO, Game } from 'phaser';
 
-//  Find out more information about the Game Config at:
-//  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
+// Find out more information about the Game Config at:
+// https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config = {
     type: AUTO,
     width: 1024,
